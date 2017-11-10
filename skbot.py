cd1fa4923b98b5c55e1444b4eaf7e1d0a30e1f8d
Christer Enfors <christer.enfors@gmail.com>
2017-11-10 16:44:07 +0100

    Add more questions

--- skbot.py
+++ skbot.py
@@ -6,7 +6,7 @@ Attempts to answer questions about Shorinji Kempo.
 
 import textwrap
 
-from enforsml.text import nlp
+from enforsml.text import nlp, utils
 
 
 def main():
@@ -20,6 +20,7 @@ def main():
                       "Vad är Shorinji Kempo för slags kampsport",
                       "Shorinji Kempo är en mångsidig japansk kampsport."],
 
+                     # Hur blir man medlem
                      ["Hur blir man medlem",
                       "Hur blir man medlem i Shorinji Kempo",
                       "Vad krävs för att bli medlem",
@@ -29,6 +30,7 @@ def main():
                       "Information om hur man blir medlem hittar du på "
                       "http://shorinjikempo.net/traning/borja-trana."],
 
+                     # Vem grundade Shorinji Kempo
                      ["Vem grundade Shorinji Kempo",
                       "Vem var det som grundade Shorinji Kempo",
                       "Vem är Shorinji Kempos grundare",
@@ -38,6 +40,7 @@ def main():
                       "Vem startade Shorinji Kempo",
                       "Shorinji Kempo grundades av So Doshin."],
 
+                     # Skillnad mot andra kampsporter
                      ["Vad skiljer Shorinji Kempo från andra kampsporter",
                       "Vad är det för skillnad mellan Shorinji Kempo och "
                       "andra kampsporter",
@@ -51,6 +54,7 @@ def main():
                       "kampsport\", eftersom vi lägger relativt stor vikt "
                       "vid filosofi."],
 
+                     # Finns det sparkar och slag
                      ["Finns det sparkar och slag i Shorinji Kempo",
                       "Finns det slag i Shorinji Kempo",
                       "Slår man i Shorinji Kempo",
@@ -59,6 +63,7 @@ def main():
                       "Shorinji Kempo innehåller sparkar, slag, "
                       "losstagningar, nedtagningar och kast."],
 
+                     # Gör det ont
                      ["Gör det ont att träna Shorinji Kempo",
                       "Får man ont av att träna",
                       "Gör det ont när man tränar",
@@ -68,6 +73,7 @@ def main():
                       "varandra. Dessa tekniker tränas dock inte i "
                       "juniorgruppen."],
 
+                     # Vad kostar det
                      ["Vad kostar det att vara medlem",
                       "Vad kostar det att bli medlem i klubben",
                       "Vad kostar träningen",
@@ -80,6 +86,7 @@ def main():
                       "Alla priser finns på "
                       "http://shorinjikempo.net/traning/borja-trana."],
 
+                     # Provträna gratis
                      ["Kan man provträna gratis",
                       "Måste man bli medlem innan man tränar",
                       "Kan man prova innan man blir medlem",
@@ -88,6 +95,7 @@ def main():
                       "Om man betalar månadsavgiften, så kan man provträna en "
                       "månad utan att behöva betala träningsavgift."],
 
+                     # Var ligger dojon
                      ["Var ligger träningslokalen",
                       "Var finns dojon",
                       "Var finns er dojo",
@@ -95,10 +103,12 @@ def main():
                       "Var finns er träningslokal",
                       "Var håller ni till",
                       "Var tränar ni",
+                      "Var finns ni",
                       "Var ligger klubbens träningslokal",
                       "Vi tränar i Kvarnbergsskolans gymnastiksal, på "
                       "Ölmegatan 10 i Karlstad."],
 
+                     # Är det svårt
                      ["Är Shorinji Kempo svårt",
                       "Är det en svår sport",
                       "Är det svårt att träna Shorinji Kempo",
@@ -108,6 +118,7 @@ def main():
                       "Det finns mycket att lära sig, men det är också det "
                       "som gör det intressant."],
 
+                     # Träningstider
                      ["Vilka träningstider har ni",
                       "När tränar ni",
                       "Vilka dagar tränar ni",
@@ -117,9 +128,12 @@ def main():
                       "Hur länge håller träningarna på",
                       "Hur många gånger tränar ni per vecka",
                       "Hur många träningspass har ni",
+                      "Hur ofta tränar ni",
+                      "Hur ofta har ni träningar",
                       "Träningstiderna hittar du på "
                       "http://shorinjikempo.net/traning/traningstider."],
 
+                     # Åldersgränser
                      ["Hur gammal måste man vara",
                       "Vad har ni för åldersgränser",
                       "Har ni någon åldersgräns",
@@ -135,6 +149,7 @@ def main():
                       "Vi har ingen övre åldersgräns, och vi har "
                       "aktiva medlemmar som är 60+."],
 
+                     # Måste man kunna japanska
                      ["Måste man kunna japanska",
                       "Behöver man lära sig japanska",
                       "Får man lära sig japanska",
@@ -142,6 +157,7 @@ def main():
                       "Shorinji Kempo, men vissa ord och fraser kommer man "
                       "att lära sig av sig själv."],
 
+                     # Vem är tränare
                      ["Vem är tränaren",
                       "Vem är instruktör",
                       "Vad har ni för tränare",
@@ -151,12 +167,15 @@ def main():
                       "Vad heter er sensei",
                       "Vilka är era instruktörer",
                       "Vem är er instruktör",
-                      "Vem är Anders",
+                      "Vem är Anders Pettersson",
                       "Vem är Anders-sensei",
+                      "Vilken grad har er tränare",
+                      "Vilken grad har er sensei",
                       "Vår huvudinstruktör heter Anders Pettersson, "
                       "och han har graden rokudan - svart bälte av "
                       "sjätte graden."],
 
+                     # När grundades klubben
                      ["När grundades klubben",
                       "Vilket år grundades klubben",
                       "Vem grundade klubben",
@@ -166,20 +185,25 @@ def main():
                       "Klubben grundades av bland andra Anders Pettersson, "
                       "1981."],
 
+                     # Hur många medlemmar har vi
                      ["Hur många medlemmar har ni",
                       "Hur stort är medlemsantalet",
                       "Hur många aktiva medlemmar har ni i klubben",
+                      "Hur många är ni",
                       "I nuläget har vi c:a 40 aktiva medlemmar i vår klubb."],
 
+                     # Hur många aktiva utövare i Sverige
                      ["Hur många aktiva finns det i Sverige",
                       "Hur många medlemmar finns det totalt i Sverige",
                       "Totalt finns det c:a 300 aktiva utövare i Sverige."],
 
+                     # Hur många klubbar finns det i Sverige
                      ["Hur många klubbar finns det i Sverige",
                       "Hur många föreningar finns det i Sverige",
                       "Det finns ett tiotal Shorinji Kempo-föreningar i "
                       "Sverige."],
 
+                     # Kontaktuppgifter
                      ["Hur kontaktar man er",
                       "Kan man ringa till er",
                       "Vad är era kontaktuppgifter",
@@ -195,6 +219,14 @@ def main():
                       "Våra kontaktuppgifter hittar du på "
                       "http://shorinjikempo.net/kontakt."],
 
+                     # Hemsida
+                     ["Var finns er hemsida",
+                      "Har ni någon hemsida",
+                      "Vad har er hemsida för adress",
+                      "Vad är er hemsideadress",
+                      "Vår hemsida finns på http://www.shorinjikempo.net."],
+
+                     # Facebook
                      ["Finns ni på Facebook",
                       "Har ni någon Facebook-sida",
                       "Har ni någon Facebook sida",
@@ -202,7 +234,88 @@ def main():
                       "Adressen till klubbens Facebook-sida"
                       "Klubbens Facebook sida",
                       "Vår Facebook-sida finns på "
-                      "http://www.facebook.com/ShorinjiKempoKarlstad."]]
+                      "http://www.facebook.com/ShorinjiKempoKarlstad."],
+
+                     # Svart bälte
+                     ["Kan man få svart bälte",
+                      "Hur lång tid tar det att få svart bälte",
+                      "När får man svart bälte",
+                      "Hur mycket träning krävs det för att få svart bälte",
+                      "Om man ligger i, kan man ta svart bälte efter ungefär "
+                      "fem års träning."],
+
+                     # Behöver man skor
+                     ["Behöver man ha skor",
+                      "Har man skor på sig",
+                      "Får man ha skor när man tränar",
+                      "Shorinji Kempo tränar man barfota,"],
+
+                     # Dogi
+                     ["Var kan man köpa kläder",
+                      "Måste man ha gi",
+                      "Kan man träna i vanliga kläder",
+                      "Kan man använda vanliga träningskläder",
+                      "Tränar ni med gi eller no-gi",
+                      "När man börjar, så kan man använda vanliga "
+                      "träningskläder. Sen kan man köpa en traditionell "
+                      "träningsdräkt - en dogi - genom klubben."],
+
+                     # Skador
+                     ["Är det lätt att skada sig",
+                      "Hur stor är skaderisken",
+                      "Är det farligt att träna",
+                      "Kan man skada sig",
+                      "Hur ofta blir folk skadade",
+                      "Hur vanligt är det med skador",
+                      "Jag är rädd att bli skadad",
+                      "Kan man bli skadad",
+                      "Skaderisken är relativt låg i Shorinji Kempo, jämfört "
+                      "med t.ex. fotboll eller innebandy."],
+
+                     # Hur många bälten finns det
+                     ["Hur många bälten finns det",
+                      "Vad finns det för bälten",
+                      "Får man ta bälten",
+                      "Alla börjar med vitt bälte. Om man är under 10 år kan "
+                      "man sedan ta gult bälte (detta bälte hoppar man över "
+                      "om man är över 10 år). Sedan kommer det grönt bälte i "
+                      "tre grader (alltså tre separata graderingar), sedan "
+                      "brunt i tre grader, och till sist svart i nio grader."],
+
+                     # Nybörjargrupp
+                     ["Har ni nybörjargrupper",
+                      "När tränar nybörjare",
+                      "Har ni träning för nybörjare",
+                      "När har ni nybörjarintag",
+                      "Kan man börja mitt i terminen",
+                      "Kan man börja mitt i en termin",
+                      "När tränar nybörjarna",
+                      "I Shorinji Kempo så tränar alla tillsammans, oavsett "
+                      "erfarenhetsnivå. Det ingår i konceptet att de erfarna "
+                      "hjälper till att instruera nybörjarna, så alla tränar "
+                      "på samma tider, oavsett nivå."],
+
+                     # Familjerabatt
+                     ["Har ni familjerabatt",
+                      "Finns det familje rabatt",
+                      "Kan man få familjerabatt",
+                      "Kan man få rabatt",
+                      "Har ni familjerabatter",
+                      "Har ni familje rabatter",
+                      "Det finns många familjer som tränar tillsammans hos "
+                      "oss, och det tycker vi är mycket positivt. Därför "
+                      "erbjuder vi familjerabatter - se "
+                      "http://www.shorinjikempo.net/traning/"
+                      "traningsavgifter/."],
+
+                     # Barngrupp
+                     ["Har ni barngrupper",
+                      "Har ni någon barngrupp",
+                      "Har ni en juniorgrupp",
+                      "Finns det någon barngrupp",
+                      "Är ni uppdelade i barngrupp och vuxengrupp",
+                      "Barn under 13 år tränar för sig, och övriga tränar i "
+                      "vuxengruppen."]]
 
     for data in training_data:
         intent = nlp.Intent(data[0])
@@ -230,11 +343,18 @@ och om Shorinji Kempo-klubben i Karlstad.""")
             user_txt = user_txt.replace("shorinjo", "Shorinji")
             user_txt = user_txt.replace("Shorinjo", "Shorinji")
 
-        try:
-            result = parser.parse(user_txt)[0]
-            print("\n".join(textwrap.wrap(result.intent.response_data)))
-        except IndexError:
-            print("Tyvärr förstår jag inte din fråga.")
+        user_txt = utils.unify_sentence_dividers(user_txt)
+        sentences = utils.normalize_and_split_sentences(user_txt)
+
+        for sentence in sentences:
+            sentence = utils.remove_junk_chars(sentence)
+
+            try:
+                result = parser.parse(sentence)[0]
+                print("[Score: %d]" % result.score)
+                print("\n".join(textwrap.wrap(result.intent.response_data)))
+            except IndexError:
+                print("Tyvärr förstår jag inte din fråga.")
 
 
 if __name__ == "__main__":
