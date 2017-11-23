6bf95ef6bb2a3ed4ba0933960d81973182e872b7
Christer Enfors <christer.enfors@gmail.com>
2017-11-23 12:59:16 +0100

    Improve error handling and logging

--- nextdemobot.py
+++ nextdemobot.py
@@ -30,17 +30,24 @@ class App(object):
             print(msg)
 
     def read_private(self, file_name):
+        """Return the contents of a "secret" file in the private/ directory.
+        We do this to be able to have the secret telegram token in a file
+        which is NOT in the source code - IE, we don't want the token to
+        be publicly available on GitHub.
+        """
         with open(file_name, "r") as f:
             contents = f.readline()
         return contents.strip()
 
     def handle_msg(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
+        try:
+            msg["message"]["chat"]
+        except KeyError:
+            print("Unknown message format.")
+            return None
 
-        if content_type != "text":
-            return False
-
-        pprint(msg)
+        msg = msg["message"]
+        
         user_txt = msg["text"].strip()
         user_id = msg["chat"]["id"]
 
@@ -71,24 +78,53 @@ class App(object):
                 self.debug_msg("\nANSWER:")
                 response = result.intent.response_data
                 self.debug_msg("    %s" % response)
-                self.bot.sendMessage(chat_id, response)
+                self.send_response(msg, response)
             except IndexError:
-                self.bot.sendMessage(chat_id, "Jag förstår tyvärr inte. "
-                                     "Kan du omformulera dig?")
+                self.send_response(msg, "Jag förstår tyvärr inte. "
+                                   "Kan du omformulera dig?")
 
         user_txt = ""
+
+    def send_msg(self, chat_id, text):
+        self.bot.sendMessage(chat_id, text)
+
+    def send_response(self, in_response_to_msg, text):
+        print("Bot -> %s %s: %s" % (in_response_to_msg["from"]["first_name"],
+                                    in_response_to_msg["from"]["last_name"],
+                                    text))
+        self.send_msg(in_response_to_msg["chat"]["id"], text)
 
     def run(self):
         self.bot = telepot.Bot(self.read_private("private/telegram_token"))
 
-        MessageLoop(self.bot, self.handle_msg).run_as_thread()
+        # MessageLoop(self.bot, self.handle_msg).run_as_thread()
 
+        update_id = None
+        wait_time = 10  # Seconds to wait before retrying after an error
+        
         while True:
             try:
-                time.sleep(10)
+                if not update_id:
+                    all_msgs = self.bot.getUpdates()
+                else:
+                    all_msgs = self.bot.getUpdates(offset=update_id)
+
+                if wait_time > 10:
+                    print("Connection re-established.")
+                    wait_time = 10
+                
+                for msg in all_msgs:
+                    update_id = msg["update_id"] + 1
+                    self.handle_msg(msg)
             except (KeyboardInterrupt):
                 print()
                 raise SystemExit
+            except:
+                print("Communication error, retrying in %d seconds." % wait_time)
+                time.sleep(wait_time)
+                wait_time *= 2
+                if wait_time > 300:
+                    wait_time = 300
 
             
 if __name__ == "__main__":
