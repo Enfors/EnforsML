e26fee4eb08a94dc816e45ccfe79426a7e01b85e
Christer Enfors <christer.enfors@gmail.com>
2017-11-23 11:45:48 +0100

    Convert script into a class

--- nextdemobot.py
+++ nextdemobot.py
@@ -3,6 +3,8 @@
 """Demo bot for CGI's Swedish Learders' conference 2017-12-01.
 """
 
+from pprint import pprint
+
 import textwrap
 import time
 
@@ -16,73 +18,79 @@ bot_file = emlb.BotFile()
 intents = bot_file.load("nextdemobot.emlb")
 
 parser = nlp.Parser(intents)
-debug=False
-
-def debug_msg(msg):
-    global debug
-
-    if debug:
-        print(msg)
 
 
-def read_private(file_name):
-    with open(file_name, "r") as f:
-        contents = f.readline()
-    return contents.strip()
+class App(object):
 
+    def __init__(self, debug=False):
+        self.debug = debug
+    
+    def debug_msg(self, msg):
+        if self.debug:
+            print(msg)
 
+    def read_private(self, file_name):
+        with open(file_name, "r") as f:
+            contents = f.readline()
+        return contents.strip()
 
-def handle_msg(msg):
-    global bot
+    def handle_msg(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
 
-    content_type, chat_type, chat_id = telepot.glance(msg)
+        if content_type != "text":
+            return False
 
-    if content_type != "text":
-        return False
+        pprint(msg)
+        user_txt = msg["text"].strip()
+        user_id = msg["chat"]["id"]
 
-    user_txt = msg["text"].strip()
-    user_id = msg["chat"]["id"]
+        if len(user_txt) < 1:
+            return False
 
-    if len(user_txt) < 1:
-        return False
+        if user_txt[0] == "/":
+            user_txt = user_txt[1:]
 
-    if user_txt[0] == "/":
-        user_txt = user_txt[1:]
+        print("Bot <- %s %s: %s" % (msg["from"]["first_name"],
+                                    msg["from"]["last_name"],
+                                    user_txt))
 
-    print("Message from user:", user_txt)
+        user_txt = utils.unify_sentence_dividers(user_txt)
+        sentences = utils.normalize_and_split_sentences(user_txt)
 
-    user_txt = utils.unify_sentence_dividers(user_txt)
-    sentences = utils.normalize_and_split_sentences(user_txt)
+        for sentence in sentences:
+            sentence = utils.remove_junk_chars(sentence)
 
-    for sentence in sentences:
-        sentence = utils.remove_junk_chars(sentence)
+            results = parser.parse(sentence)
 
-        results = parser.parse(sentence)
-
-        debug_msg("ANSWER CANDIDATES:")
-        for result in results:
-            debug_msg("    [%3d] %s" % (result.score, result.intent.response_data))
+            self.debug_msg("ANSWER CANDIDATES:")
+            for result in results:
+                self.debug_msg("    [%3d] %s" % (result.score, result.intent.response_data))
         
-        try:
-            result = results[0]
-            debug_msg("\nANSWER:")
-            response = result.intent.response_data
-            bot.sendMessage(chat_id, response)
-        except IndexError:
-            bot.sendMessage(chat_id, "Jag förstår tyvärr inte. "
-                            "Kan du omformulera dig?")
+            try:
+                result = results[0]
+                self.debug_msg("\nANSWER:")
+                response = result.intent.response_data
+                self.debug_msg("    %s" % response)
+                self.bot.sendMessage(chat_id, response)
+            except IndexError:
+                self.bot.sendMessage(chat_id, "Jag förstår tyvärr inte. "
+                                     "Kan du omformulera dig?")
 
-    user_txt = ""
+        user_txt = ""
 
+    def run(self):
+        self.bot = telepot.Bot(self.read_private("private/telegram_token"))
 
-bot = telepot.Bot(read_private("private/telegram_token"))
-print(bot.getMe())
+        MessageLoop(self.bot, self.handle_msg).run_as_thread()
 
-MessageLoop(bot, handle_msg).run_as_thread()
+        while True:
+            try:
+                time.sleep(10)
+            except (KeyboardInterrupt):
+                print()
+                raise SystemExit
 
-while True:
-    try:
-        time.sleep(10)
-    except (KeyboardInterrupt):
-        print()
-        raise SystemExit
+            
+if __name__ == "__main__":
+    app = App()
+    app.run()
